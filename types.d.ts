ab04f37ab8be8f6d9bbbbc665aaf4bf432dc1e83
feysj7 <f65451616@gmail.com>
2026-04-09 01:35:08 +0500

    Add files via upload

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,9 @@
-import * as Ajv from 'ajv';
+import { Type } from 'arktype';
 import { FieldValues, ResolverOptions, ResolverResult } from 'react-hook-form';
-export type Resolver = <T>(schema: Ajv.JSONSchemaType<T>, schemaOptions?: Ajv.Options, factoryOptions?: {
-    mode?: 'async' | 'sync';
-}) => <TFieldValues extends FieldValues, TContext>(values: TFieldValues, context: TContext | undefined, options: ResolverOptions<TFieldValues>) => Promise<ResolverResult<TFieldValues>>;
+export type Resolver = <T extends Type<any>>(schema: T, schemaOptions?: undefined, factoryOptions?: {
+    /**
+     * Return the raw input values rather than the parsed values.
+     * @default false
+     */
+    raw?: boolean;
+}) => <TFieldValues extends FieldValues, TContext>(values: TFieldValues, context: TContext | undefined, options: ResolverOptions<TFieldValues>) => ResolverResult<TFieldValues>;
